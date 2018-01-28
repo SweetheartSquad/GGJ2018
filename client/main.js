50b9e47812ce8bd11b5cf04887635dad6f525fa5
Sean LeBlanc <sean.s.leblanc@gmail.com>
2018-01-28 15:10:14 -0500

    mouse locking

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ function init(){
 	keys.init({
 		capture: [keys.LEFT,keys.RIGHT,keys.UP,keys.DOWN,keys.SPACE,keys.ENTER,keys.BACKSPACE,keys.ESCAPE,keys.W,keys.A,keys.S,keys.D,keys.P,keys.M]
 	});
-	mouse.init("canvas", false);
+	mouse.init("canvas", true);
 
 	// setup main loop
 	var main = function(){
