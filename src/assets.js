63d3ddc24df2053f02e9987862f8e5c027b13c39
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2019-08-29 01:38:49 -0400

    fix font issue

--- src/assets.js
+++ src/assets.js
@@ -1,8 +1,8 @@
 import * as textures from './assets/textures/textures';
 import * as audio from './assets/audio/audio';
 
-import fontImg from './assets/font/font.fnt.png'; // not using the image directly, but need to make sure it's imported for the xml to reference
-import font from './assets/font/font.fnt.xml';
+import fontImg from './assets/font.fnt.png'; // not using the image directly, but need to make sure it's imported for the xml to reference
+import font from './assets/font.fnt.xml';
 import road_shader from './assets/road_shader.frag';
 import screen_shader from './assets/screen_shader.frag';
 
